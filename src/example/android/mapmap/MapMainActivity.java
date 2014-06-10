287f7d48fbb595626ea0038ddfb014eb7a4084e5
7799 <hayashi7799@gmail.com>
2014-06-10 13:45:55 +0900

    init

---
+++ src/example/android/mapmap/MapMainActivity.java
@@ -0,0 +1,17 @@
+package example.android.mapmap;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+
+public class MapMainActivity extends FragmentActivity {
+	 
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+ 
+}
+
